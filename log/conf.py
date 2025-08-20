65c8a4a0e1f59e3c760322634b45d82fab8bff19
RuslanChukavin <r66cha@gmail.com>
2025-08-20 18:09:27 +0300

    add FORMAT and DATE_FORMAT constants ad conf.py

--- log/conf.py
+++ log/conf.py
@@ -10,10 +10,17 @@ __all__ = ["conf_logging"]
 
 # --
 
+DATE_FORMAT: str = "%Y-%m-%d %H:%M:%S"
+FORMAT: str = (
+    "[%(asctime)s.%(msecs)03d] %(module)-8s:%(lineno)-8d %(levelname)-8s - %(message)s"
+)
+
+# --
+
 
 def conf_logging(level=logging.INFO):
     logging.basicConfig(
         level=level,
-        datefmt="%Y-%m-%d %H:%M:%S",
-        format="[%(asctime)s.%(msecs)03d] %(module)-8s:%(lineno)-8d %(levelname)-8s - %(message)s",
+        datefmt=DATE_FORMAT,
+        format=FORMAT,
     )
